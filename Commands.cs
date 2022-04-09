a36b82d701418f92d6af80096152161bcadb2015
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2022-04-09 15:30:16 -0400

    Add project files.

---
+++ Commands.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace _7DayBot
+{
+        public class ExampleUngrouppedCommands : BaseCommandModule
+        {
+            [Command("!restart")] // let's define this method as a command
+            [Description("Restarts the 7dtd server")] // this will be displayed to tell users what this command does when they invoke help
+            public async Task Restart(CommandContext ctx) // this command takes no arguments
+            {
+            bool allclosed = true;
+            // First find the process
+            Process[] processes = Process.GetProcessesByName("7DaysToDieServer");
+                await ctx.RespondAsync("There were " + processes.Length + " processes running...Attempting to close them all....Please Wait.");
+                foreach(Process p in processes)
+                {
+                    if(!p.HasExited)
+                    {
+                        p.CloseMainWindow();
+                    }
+                }
+                await Task.Delay(10000);
+                foreach(Process p in processes)
+                {
+                    if(!p.HasExited)
+                    {
+                        allclosed = false;
+                        await ctx.RespondAsync("Process id " + p.Id + " did not close gracefully. Will try to force");
+                        p.Kill();
+                    }
+                }
+                if(!allclosed)
+                {
+                    await Task.Delay(5000);
+                    allclosed = true;
+                    foreach (Process p in processes)
+                    {
+                        if (!p.HasExited)
+                        {
+                            allclosed = false;
+                        }
+                    }
+                }
+                if(!allclosed)
+                {
+                    await ctx.RespondAsync("Could not restart server, notify admin...");
+                    return;
+                }
+                await ctx.RespondAsync("Restarting Server...");
+                System.Diagnostics.Process.Start(@"C:\Program Files(x86)\Steam\steamapps\common\7 Days to Die Dedicated Server\startdedicated.bat");
+                await Task.Delay(5000);
+                processes = Process.GetProcessesByName("7DaysToDieServer");
+                if(processes.Length > 0)
+                {
+                    await ctx.RespondAsync("Server has restarted...");
+                }
+                return;
+            }
+
+        }
+ }
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DSPlus.Examples
+{
+    public class Program
+    {
+        public readonly EventId BotEventId = new EventId(42, "Bot-Ex01");
+
+        public DiscordClient Client { get; set; }
+
+        public static void Main(string[] args)
+        {
+            // since we cannot make the entry method asynchronous,
+            // let's pass the execution to asynchronous code
+            var prog = new Program();
+            prog.RunBotAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task RunBotAsync()
+        {
+            // first, let's load our configuration file
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            // next, let's load the values from that file
+            // to our client's configuration
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            var cfg = new DiscordConfiguration
+            {
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            // then we want to instantiate our client
+            this.Client = new DiscordClient(cfg);
+
+            // next, let's hook some events, so we know
+            // what's going on
+            this.Client.Ready += this.Client_Ready;
+            this.Client.GuildAvailable += this.Client_GuildAvailable;
+            this.Client.ClientErrored += this.Client_ClientError;
+
+            // finally, let's connect and log in
+            await this.Client.ConnectAsync();
+
+            // and this is to prevent premature quitting
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            // let's log the fact that this event occured
+            sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            // let's log the name of the guild that was just
+            // sent to our client
+            sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            // let's log the details of the error that just 
+            // occured in our client
+            sender.Logger.LogError(BotEventId, e.Exception, "Exception occured");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+    }
+
+    // this structure will hold data from config.json
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
