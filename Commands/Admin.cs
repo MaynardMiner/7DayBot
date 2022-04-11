951e039a39e4bba41af69550ee1c0f5abb06ae79
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2022-04-11 15:03:36 -0400

    Remove no longer needed

--- Commands/Admin.cs
+++ Commands/Admin.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using _7DayBot;
 using DSharpPlus.Entities;
 using Commands.Globals;
 
